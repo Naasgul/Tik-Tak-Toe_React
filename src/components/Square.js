72c69c27778543c9a2c5263df38c2599f6da485d
Naasgul <lnaas@hotmail.de>
2020-10-26 17:15:37 +0100

    Add completing the game which doesnt work

--- src/components/Square.js
+++ src/components/Square.js
@@ -5,6 +5,10 @@ import React from "react";
   return <button className="square">{props.value}</button>;
 } */
 
-export default function Square({ value }) {
-  return <button className="square">{value}</button>;
+export default function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onclick()}>
+      {props.value}
+    </button>
+  );
 }
