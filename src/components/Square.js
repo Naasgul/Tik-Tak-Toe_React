932bd92dc69d8b456409070216830d251de435d4
Naasgul <lnaas@hotmail.de>
2020-10-26 15:29:52 +0100

    Passing data through props

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,10 @@
 import "./square.css";
 import React from "react";
 
-export default function Square() {
-  return <button className="square">{/* TODO */}</button>;
+/* export default function Square(props) {
+  return <button className="square">{props.value}</button>;
+} */
+
+export default function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
